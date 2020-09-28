ec03e773a63cebdb5cbbf8d0cfbd78bbec363879
Eduard Knezovic <e.knezovic@kendaxa.com>
2020-09-28 23:16:55 +0200

    polish

--- README.md
+++ README.md
@@ -7,3 +7,11 @@ To run the project:
 ```docker-compose up --build```
 
 Database creation and migration will be done automatically.
+
+## Access
+
+Can be accessed here:
+
+```
+http://localhost:1337
+```
--- project/suntime/urls.py
+++ project/suntime/urls.py
@@ -1,9 +1,8 @@
 
 from django.urls import path
-from .views import index, get_sunrise_sunset, vue_suntime
+from .views import index, get_sunrise_sunset
 
 urlpatterns = [
-    path("", index, name="suntime-index"),
+    path("", index, name="suntime"),
     path("get_sunrise_sunset", get_sunrise_sunset, name="get_sunrise_sunset"),
-    path("vue_suntime", vue_suntime, name="vue_suntime"),
 ]
